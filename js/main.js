5f463f40ef83339336d347fbe3b95ff0b77c0428
jamarri <jamarri@marketdata.com.br>
2015-07-08 11:59:05 -0300

    ajax loading

--- js/main.js
+++ js/main.js
@@ -38,6 +38,28 @@ app.factory("ShotDetail", function($resource) {
 });
 
 
+ app.directive('loading',   ['$http' ,function ($http) {
+        return {
+            restrict: 'A',
+            link: function (scope, elm, attrs)
+            {
+                scope.isLoading = function () {
+                    return $http.pendingRequests.length > 0;
+                };
+
+                scope.$watch(scope.isLoading, function (v)
+                {
+                    if(v){
+                        elm.show();
+                    }else{
+                        elm.hide();
+                    }
+                });
+            }
+        };
+
+ }]);
+
 /**
  * Controls all other Pages
  */
